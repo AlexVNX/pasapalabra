a8102e4ac04de88008e40e4ce0f02aec57af9492
AlexVNX <129174676+AlexVNX@users.noreply.github.com>
2026-01-29 22:03:50 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { renderHome } from "./modes/home.js";
 import { renderStudy } from "./modes/study.js";
 import { renderPasapalabra } from "./modes/pasapalabra.js";
 import { renderEditor } from "./modes/editor.js";
+import { renderRanking } from "./modes/ranking.js";
 
 export function mountRoute(){
   const app = document.getElementById("app");
@@ -12,5 +13,6 @@ export function mountRoute(){
   if (hash === "#/study") return renderStudy(app);
   if (hash === "#/pasapalabra") return renderPasapalabra(app);
   if (hash === "#/editor") return renderEditor(app);
+  if (hash === "#/ranking") return renderRanking(app);
   return renderHome(app);
 }
